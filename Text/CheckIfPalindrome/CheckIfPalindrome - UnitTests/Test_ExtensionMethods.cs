2f0684c538bd2fd729eb85b8dec5546c049ad57c
nagmagit <rossogiuliano@gmail.com>
2020-01-22 22:39:36 -0300

    Project started and unit tests created - Check if palindrome

---
+++ Text/CheckIfPalindrome/CheckIfPalindrome - UnitTests/Test_ExtensionMethods.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Nagma.CheckIfPalindrome
+{
+    [TestFixture]
+    public class Test_ExtensionMethods
+    {
+        [Test]
+        public void IsPalindrome_NonPalindromeNormalWord_ReturnsFalse()
+        {
+            string input = "aeea yo soy sabalero";
+
+            bool actual = input.IsPalindrome();
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsPalindrome_PalindromeNormalWord_ReturnsTrue()
+        {
+            string input = "anitalavalatina";
+
+            bool actual = input.IsPalindrome();
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsPalindrome_NonPalindromeWithCombinedChars_ReturnsFalse()
+        {
+            string input = "😎🦅";
+
+            bool actual = input.IsPalindrome();
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void IsPalindrome_PalindromeWithCombinedChars_ReturnsTrue()
+        {
+            // Depending on the IDE, it may not be noticeable that the faces
+            // get increasingly paler towards the middle. This is to test
+            // the compatibility with Unicode's combined characters.
+            string input = "👨🏿👨🏾👨🏽👨🏼👨🏻👨👨🏻👨🏼👨🏽👨🏾👨🏿";
+
+            bool actual = input.IsPalindrome();
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsPalindrome_EmptyString_ReturnsTrue()
+        {
+            string input = String.Empty;
+
+            bool actual = input.IsPalindrome();
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsPalindrome_NullString_ThrowsNullReferenceException()
+        {
+            string input = null;
+
+            TestDelegate test = (() => input.IsPalindrome());
+
+            Assert.Throws<NullReferenceException>(test);
+        }
+    }
+}
---
+++ Text/CheckIfPalindrome/CheckIfPalindrome/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nagma.CheckIfPalindrome
+{
+    public static class Helpers
+    {
+        public static bool IsPalindrome(this string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
