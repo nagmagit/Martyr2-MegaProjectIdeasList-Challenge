25a67ed78f6d475e97fe6b6455684a44fd0b2956
nagmagit <rossogiuliano@gmail.com>
2020-01-24 16:21:15 -0300

    First implementation of String.IsPalindrome, it doesn't like emoji though - Check if palindrome

--- Text/CheckIfPalindrome/CheckIfPalindrome/Helpers.cs
+++ Text/CheckIfPalindrome/CheckIfPalindrome/Helpers.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Linq;
 
 namespace Nagma.CheckIfPalindrome
 {
     public static class Helpers
     {
+        /// <summary>
+        /// Checks if a string is palindrome; that is, if the string reads
+        /// the same in reverse.
+        /// </summary>
+        /// <param name="text">The string to check.</param>
+        /// <exception cref="NullReferenceException"></exception>
         public static bool IsPalindrome(this string text)
         {
-            throw new NotImplementedException();
+            if (text is null) throw new NullReferenceException("Input string can't be null.");
+
+            string reversedText = String.Join(String.Empty, text.Reverse());
+
+            return (reversedText == text);
         }
     }
 }
