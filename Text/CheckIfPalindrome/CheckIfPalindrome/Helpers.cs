ca5dce3c21fc509a8d089d9e169b1e2b23cfec9e
nagmagit <rossogiuliano@gmail.com>
2020-01-28 03:35:34 -0300

    Added support for up to 2 joined emojis - Check if palindrome

--- Text/CheckIfPalindrome/CheckIfPalindrome/Helpers.cs
+++ Text/CheckIfPalindrome/CheckIfPalindrome/Helpers.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using NeoSmart.Unicode;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 
 namespace Nagma.CheckIfPalindrome
 {
@@ -15,9 +19,53 @@ namespace Nagma.CheckIfPalindrome
         {
             if (text is null) throw new NullReferenceException("Input string can't be null.");
 
-            string reversedText = String.Join(String.Empty, text.Reverse());
+            string reversedText = ReverseUnicode(text);
 
             return (reversedText == text);
+        }
+
+        private static string ReverseUnicode(string text)
+        {
+            List<string> lettersList = new List<string>();
+
+            Codepoint[] codepoints = text.Codepoints().ToArray();
+
+            for (int i = 0; i < codepoints.Length; i++)
+            {
+                // If this is the last codepoint in the string
+                if (i + 1 >= codepoints.Length)
+                {
+                    lettersList.Add(codepoints[i].AsString());
+                    continue;
+                }
+
+                Codepoint codepoint = codepoints[i];
+                Codepoint nextCodepoint = codepoints[i+1];
+
+                // If it's an actual emoji, add it combined
+                if (TryCombinedEmoji(out SingleEmoji combinedEmoji, codepoint, nextCodepoint))
+                {
+                    i++;    // Skip next emoji
+                    lettersList.Add(combinedEmoji.ToString());
+                }
+                else
+                {
+                    lettersList.Add(codepoint.AsString());
+                }
+            }
+
+            lettersList.Reverse();
+
+            return String.Join(String.Empty, lettersList);
+        }
+
+        private static bool TryCombinedEmoji(out SingleEmoji combinedEmoji, params Codepoint[] codepoints)
+        {
+            // Attemp to create a single emoji by joining them all
+            combinedEmoji = new SingleEmoji(new UnicodeSequence(codepoints), String.Empty, null, -1);
+
+            // Check if the resulting emoji actually exists
+            return Emoji.All.Contains(combinedEmoji);
         }
     }
 }
